19fb2a5e1da2dc310eb1b9d33c38eda12e93f09f
awolferret <104518827+awolferret@users.noreply.github.com>
2022-07-11 22:00:00 +0300

    4

--- HealthBar/Assets/Scripts/Health.cs
+++ HealthBar/Assets/Scripts/Health.cs
@@ -1,49 +1,21 @@
-using System.Collections;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
     private float _health;
-    private float _targetHealth;
-    private float _scale = 0.1f;
-    private Coroutine _coroutine;
 
     public void Heal(float healAmount)
     {
-        _targetHealth = _health + healAmount;
-        StopCoroutine();
-        _coroutine = StartCoroutine(ChangeHealth(_targetHealth));
+        _health += healAmount;
     }
 
     public void Damage(float damageAmount)
     {
-        _targetHealth = _health - damageAmount;
-        StopCoroutine();
-        _coroutine = StartCoroutine(ChangeHealth(_targetHealth));
+        _health -= damageAmount;
     }
 
     public float ShowHealth()
     {
         return _health;
-    }
-
-    private void StopCoroutine()
-    {
-        if (_coroutine != null)
-        {
-            StopCoroutine(_coroutine);
-        }
-    }
-
-    private IEnumerator ChangeHealth(float targetHealth)
-    {
-        float waitTime = 0.01f;
-        var waitType = new WaitForSeconds(waitTime);
-
-        while (_health != targetHealth)
-        {
-            _health = Mathf.MoveTowards(_health, targetHealth, _scale);
-            yield return waitType;
-        }
     }
 }
--- HealthBar/Assets/Scripts/Visual.cs
+++ HealthBar/Assets/Scripts/Visual.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,14 +9,28 @@ public class Visual : MonoBehaviour
     [SerializeField] private Slider _slider;
 
     private Health _health;
+    private Coroutine _coroutine;
 
     private void Start()
     {
         _health = GetComponent<Health>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        _slider.value = _health.ShowHealth();
+        _coroutine = StartCoroutine(ChangeHealth(_health.ShowHealth()));
+    }
+
+    private IEnumerator ChangeHealth(float targetHealth)
+    {
+        float waitTime = 0.01f;
+        var waitType = new WaitForSeconds(waitTime);
+        float _scale = 0.01f;
+
+        while (_slider.value != targetHealth)
+        {
+            _slider.value = Mathf.MoveTowards(_slider.value, targetHealth, _scale);
+            yield return waitType;
+        }
     }
 }
