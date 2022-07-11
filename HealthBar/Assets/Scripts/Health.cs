912c4e4748dc0861ff7691f0b6e3eb2ce6d391fb
awolferret <104518827+awolferret@users.noreply.github.com>
2022-07-11 21:30:32 +0300

    3

--- HealthBar/Assets/Scripts/Health.cs
+++ HealthBar/Assets/Scripts/Health.cs
@@ -1,19 +1,49 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
     private float _health;
-    private float _healthChange = 10f;
+    private float _targetHealth;
+    private float _scale = 0.1f;
+    private Coroutine _coroutine;
 
-    public void Damage()
+    public void Heal(float healAmount)
     {
-        _health -= _healthChange;
+        _targetHealth = _health + healAmount;
+        StopCoroutine();
+        _coroutine = StartCoroutine(ChangeHealth(_targetHealth));
     }
 
-    public void Heal()
+    public void Damage(float damageAmount)
     {
-        _health += _healthChange;
+        _targetHealth = _health - damageAmount;
+        StopCoroutine();
+        _coroutine = StartCoroutine(ChangeHealth(_targetHealth));
+    }
+
+    public float ShowHealth()
+    {
+        return _health;
+    }
+
+    private void StopCoroutine()
+    {
+        if (_coroutine != null)
+        {
+            StopCoroutine(_coroutine);
+        }
+    }
+
+    private IEnumerator ChangeHealth(float targetHealth)
+    {
+        float waitTime = 0.01f;
+        var waitType = new WaitForSeconds(waitTime);
+
+        while (_health != targetHealth)
+        {
+            _health = Mathf.MoveTowards(_health, targetHealth, _scale);
+            yield return waitType;
+        }
     }
 }
--- HealthBar/Assets/Scripts/Visual.cs
+++ HealthBar/Assets/Scripts/Visual.cs
@@ -1,45 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
+[RequireComponent(typeof(Health))]
 
 public class Visual : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
 
-    private float _scale = 0.1f;
-    private Coroutine _coroutine;
-    private float _changeShow = 10f;
+    private Health _health;
 
-    public void ShowHeal()
+    private void Start()
     {
-        CheckCoroutine();
-        _coroutine = StartCoroutine(ShowChange(_slider.value + _changeShow));
+        _health = GetComponent<Health>();
     }
 
-    public void ShowDamage()
+    private void Update()
     {
-        CheckCoroutine();
-        _coroutine = StartCoroutine(ShowChange(_slider.value - _changeShow));
-    }
-
-    private void CheckCoroutine()
-    {
-        if (_coroutine != null)
-        {
-            StopCoroutine(_coroutine);
-        }
-    }
-
-    private IEnumerator ShowChange(float targetHealth)
-    {
-        float waitTime = 0.01f;
-        var waitType = new WaitForSeconds(waitTime);
-
-        while (_slider.value != targetHealth)
-        {
-            _slider.value = Mathf.MoveTowards(_slider.value, targetHealth, _scale);
-            yield return waitType;
-        }
+        _slider.value = _health.ShowHealth();
     }
 }
