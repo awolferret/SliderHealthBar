1bf975bcfe9e9d6407767d86f3298629e436794e
awolferret <104518827+awolferret@users.noreply.github.com>
2022-07-13 19:52:45 +0300

    5

--- HealthBar/Assets/Scripts/Health.cs
+++ HealthBar/Assets/Scripts/Health.cs
@@ -1,17 +1,22 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
+    [SerializeField] private UnityEvent _healthChanged;
+
     private float _health;
 
     public void Heal(float healAmount)
     {
         _health += healAmount;
+        _healthChanged.Invoke();
     }
 
     public void Damage(float damageAmount)
     {
         _health -= damageAmount;
+        _healthChanged.Invoke();
     }
 
     public float ShowHealth()
